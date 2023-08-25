bccb81535ebb08922fa13e98301464ef7427dc14
mladen.djuric <mladen.djuric@devtechgroup.com>
2023-08-25 16:36:11 +0200

    minor changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useEffect, useState } from 'react';
 
 export async function getServerSideProps() {
   const data = JSON.stringify({ time: new Date() });
